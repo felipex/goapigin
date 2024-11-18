7d222ff7cf1ded809b7758aea69021b614a1c03c
felipex <felipexcavalcante@gmail.com>
2024-11-17 21:48:08 -0300

    Outro endpoint

--- main.go
+++ main.go
@@ -8,16 +8,23 @@ func ping(c *gin.Context) {
 	})
 }
 
-func servidor(c *gin.Context) {
+func servidor1(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"siape": "123456",
-		"nome":  "gasparzinho",
+		"nome":  "Gasparzinho",
+	})
+}
+func servidor2(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"siape": "234567",
+		"nome":  "Assombroso",
 	})
 }
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", ping)
-	r.GET("/servidor", servidor)
+	r.GET("/servidor/1", servidor1)
+	r.GET("/servidor/2", servidor2)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
