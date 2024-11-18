f7485626bc1046ad09573991181ef27e25bd23af
felipex <felipexcavalcante@gmail.com>
2024-11-17 21:33:27 -0300

    Endpoint servidor

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func servidor(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"siape": "123456",
+		"nome":  "gasparzinho",
+	})
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", ping)
+	r.GET("/servidor", servidor)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
